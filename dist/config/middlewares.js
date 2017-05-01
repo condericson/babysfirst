21d35fff24e55bcc65a2c866d4f64f29d5e137a5
condericson@gmail.com <condericson@gmail.com>
2017-05-01 09:00:00 -0400

    http vs https

--- dist/config/middlewares.js
+++ dist/config/middlewares.js
@@ -13,7 +13,7 @@ module.exports = app => {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(morgan('dev'));
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://localhost:3000');
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
     res.header('Access-Control-Allow-Credentials', 'true');
