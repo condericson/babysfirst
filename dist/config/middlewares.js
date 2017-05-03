cfb78764dc07c2e9131bc06a8f94630ee7b65114
condericson@gmail.com <condericson@gmail.com>
2017-05-03 08:19:23 -0400

    added cors, helmet, compression

--- dist/config/middlewares.js
+++ dist/config/middlewares.js
@@ -4,6 +4,9 @@ const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
 
 module.exports = app => {
   app.use(express.static('public'));
@@ -12,11 +15,7 @@ module.exports = app => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(morgan('dev'));
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-  });
+  app.use(cors());
+  app.use(helmet());
+  app.use(compression());
 };
